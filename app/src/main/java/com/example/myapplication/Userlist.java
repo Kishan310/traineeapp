594192bcb4f680c9992e4c9dee4bf4b62733b416
Kishan Hothi <skhothisurat@gmail.com>
2019-06-11 16:08:59 +0530

    initiation file

---
+++ app/src/main/java/com/example/myapplication/Userlist.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class Userlist extends AppCompatActivity {
+
+    private List<User> listUsers;
+    private DatabaseHelper databaseHelper;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userlist);
+        databaseHelper = new DatabaseHelper(this);
+
+    }
+    private void getDataFromSQLite() {
+        // AsyncTask is used that SQLite operation not blocks the UI Thread.
+        new AsyncTask<Void, Void, Void>() {
+            private ArrayAdapter usersRecyclerAdapter;
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                listUsers.clear();
+                listUsers.addAll(databaseHelper.getAllUser());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                usersRecyclerAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+    }
+}
