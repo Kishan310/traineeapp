3345b32c5454f0c7903f543ba8eff6285fe15f0a
Kishan Hothi <skhothisurat@gmail.com>
2019-06-13 16:39:24 +0530

    modified file

--- app/src/main/java/com/example/myapplication/Userlist.java
+++ app/src/main/java/com/example/myapplication/Userlist.java
@@ -1,17 +1,25 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Userlist extends AppCompatActivity {
 
     private List<User> listUsers;
     private DatabaseHelper databaseHelper;
+    private RecyclerView recyclerViewUsers;
+    private AppCompatTextView textViewfirstname;
+    private AppCompatTextView textViewlastname;
+    private AppCompatTextView textViewemailid;
+
 
 
 
@@ -21,6 +29,31 @@ public class Userlist extends AppCompatActivity {
         setContentView(R.layout.activity_userlist);
         databaseHelper = new DatabaseHelper(this);
 
+    }
+
+    private void initViews() {
+        textViewfirstname = (AppCompatTextView) findViewById(R.id.txt_firstname);
+        textViewlastname = (AppCompatTextView) findViewById(R.id.txt_lastname);
+        textViewemailid = (AppCompatTextView) findViewById(R.id.txt_emailid);
+        recyclerViewUsers = (RecyclerView) findViewById(R.id.recyclerViewUsers);
+    }
+
+
+    private void initObjects() {
+        listUsers = new ArrayList<>();
+        usersRecyclerAdapter = new UsersRecyclerAdapter(listUsers);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewUsers.setLayoutManager(mLayoutManager);
+        recyclerViewUsers.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewUsers.setHasFixedSize(true);
+        recyclerViewUsers.setAdapter(usersRecyclerAdapter);
+        databaseHelper = new DatabaseHelper(activity);
+
+        String emailFromIntent = getIntent().getStringExtra("EMAIL");
+        textViewName.setText(emailFromIntent);
+
+        getDataFromSQLite();
     }
 
     private void getDataFromSQLite() {
