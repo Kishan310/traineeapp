fc32f865ed8ce74dc0b13b9aa34cb54b26e464ef
Kishan Hothi <skhothisurat@gmail.com>
2019-07-26 17:26:49 +0530

    display post & put response

--- app/src/main/java/com/example/myapplication/WebServicesActivity.java
+++ app/src/main/java/com/example/myapplication/WebServicesActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -17,116 +17,35 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class WebServicesActivity extends AppCompatActivity implements View.OnClickListener{
-//    private String url = "https://reqres.in/api/users?page=2";
-    private Button btnget,btnpost,btnput,btndelete;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WebServicesActivity extends AppCompatActivity implements View.OnClickListener {
+    //    private String url = "https://reqres.in/api/users?page=2";
+    EditText edtName, edtJob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_services);
 
-       init();
+        init();
 
     }
-    private void init(){
+
+    private void init() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       findViewById(R.id.btn_get).setOnClickListener(this);
-       findViewById(R.id.btn_post).setOnClickListener(this);
-       findViewById(R.id.btn_put).setOnClickListener(this);
-       findViewById(R.id.btn_delete).setOnClickListener(this);
+        edtJob=findViewById(R.id.edt_job);
+        edtName=findViewById(R.id.edt_name);
+        findViewById(R.id.btn_get).setOnClickListener(this);
+        findViewById(R.id.btn_post).setOnClickListener(this);
+        findViewById(R.id.btn_put).setOnClickListener(this);
+        findViewById(R.id.btn_delete).setOnClickListener(this);
 
-    }
-
-    private void getResponse(String url) {
-
-        RequestQueue mRequestQueue;
-        StringRequest mStringRequest;
-        mRequestQueue = Volley.newRequestQueue(this);
-        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-                Log.e("onResponse: ",response );
-                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                Log.e("Error :", error.toString());
-            }
-        });
-        mRequestQueue.add(mStringRequest);
-    }
-
-    private void postResponse(String url){
-        RequestQueue mRequestQueue;
-        StringRequest mStringRequest;
-        mRequestQueue = Volley.newRequestQueue(this);
-        mStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-                Log.e("onResponse: ",response );
-                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                Log.e("Error :", error.toString());
-            }
-        });
-        mRequestQueue.add(mStringRequest);
-    }
-
-    private void putResponse(String url){
-        RequestQueue mRequestQueue;
-        StringRequest mStringRequest;
-        mRequestQueue = Volley.newRequestQueue(this);
-        mStringRequest = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-                Log.e("onResponse: ",response );
-                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                Log.e("Error :", error.toString());
-            }
-        });
-        mRequestQueue.add(mStringRequest);
-    }
-
-    private void deleteResponse(String url){
-        RequestQueue mRequestQueue;
-        StringRequest mStringRequest;
-        mRequestQueue = Volley.newRequestQueue(this);
-        mStringRequest = new StringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-                Log.e("onResponse: ",response );
-                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                Log.e("Error :", error.toString());
-            }
-        });
-        mRequestQueue.add(mStringRequest);
     }
 
     @Override
@@ -136,7 +55,7 @@ public class WebServicesActivity extends AppCompatActivity implements View.OnCli
                 getResponse("https://reqres.in/api/users?page=2");
                 break;
             case R.id.btn_post:
-                postResponse("https://reqres.in/api/users");
+                postService("https://reqres.in/api/users");
                 break;
             case R.id.btn_put:
                 putResponse("https://reqres.in/api/users/2");
@@ -144,6 +63,134 @@ public class WebServicesActivity extends AppCompatActivity implements View.OnCli
                 deleteResponse("https://reqres.in/api/users/2");
         }
     }
+
+    private void getResponse(String url) {
+        RequestQueue mRequestQueue;
+        StringRequest mStringRequest;
+        mRequestQueue = Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("onResponse: ", response);
+                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("Error :", error.toString());
+                Toast.makeText(WebServicesActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        mRequestQueue.add(mStringRequest);
+    }
+
+    private void postService(String url) {
+        final String name = edtName.getText().toString().trim();
+        final String job = edtJob.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(WebServicesActivity.this, response, Toast.LENGTH_LONG).show();
+                        parsePostResponse(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(WebServicesActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("job", job);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void parsePostResponse(String response) {
+        try {
+            JSONObject responseJsonObject = new JSONObject(response);
+            String createdAt = responseJsonObject.getString("createdAt");
+            edtName.setText(createdAt);
+        } catch (Exception e) {
+            Log.e("parsePOSTResponse", e.toString());
+        }
+    }
+
+    private void putResponse(String url) {
+        final String name = edtName.getText().toString().trim();
+        final String job = edtJob.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
+                parsePutResponse(response);
+            }
+        },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(WebServicesActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("job", job);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void parsePutResponse(String response) {
+        try {
+            JSONObject responseJsonObject = new JSONObject(response);
+            String createdAt = responseJsonObject.getString("createdAt");
+            edtName.setText(createdAt);
+        } catch (Exception e) {
+            Log.e("parsePUTResponse", e.toString());
+        }
+    }
+
+    private void deleteResponse(String url) {
+        RequestQueue mRequestQueue;
+        StringRequest mStringRequest;
+        mRequestQueue = Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("onResponse: ", response);
+                Toast.makeText(getApplicationContext(), "Response :" + response, Toast.LENGTH_LONG).show();//display the response on screen
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("Error :", error.toString());
+            }
+        });
+        mRequestQueue.add(mStringRequest);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
