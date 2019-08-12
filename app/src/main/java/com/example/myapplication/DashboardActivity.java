65a7afd612d134ce3ba8e2dff9c8e7ff81c79748
Kishan Hothi <skhothisurat@gmail.com>
2019-08-12 17:23:51 +0530

    working on alert dialogbox

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -18,6 +17,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -29,18 +29,18 @@ import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    public void open(View view){
+    public void open(View view) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setMessage("Are you sure, You wanted to make decision");
-                alertDialogBuilder.setPositiveButton("yes",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface arg0, int arg1) {
-                                Toast.makeText(DashboardActivity.this,"You clicked yes button",Toast.LENGTH_LONG).show();
-                            }
-                        });
+        alertDialogBuilder.setPositiveButton("yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(DashboardActivity.this, "You clicked yes button", Toast.LENGTH_LONG).show();
+                    }
+                });
 
-        alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 finish();
@@ -62,8 +62,7 @@ public class DashboardActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -102,15 +101,36 @@ public class DashboardActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        } if (id == R.id.action_logout) {
-            Intent intent = new Intent(this,SigninActivity.class);
-            startActivity(intent);
+        }
+        if (id == R.id.action_logout) {
+            logoutPopUP();
             return true;
-
-
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void logoutPopUP() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Are you sure, You wanted to make decision");
+        alertDialogBuilder.setPositiveButton("yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent intent = new Intent(DashboardActivity.this, SigninActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -121,38 +141,38 @@ public class DashboardActivity extends AppCompatActivity
 
         if (id == R.id.nav_profile) {
 
-            Intent intent =  new Intent(this, ProfileActivity.class);
+            Intent intent = new Intent(this, ProfileActivity.class);
             startActivity(intent);
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_service) {
 
-            Intent intent = new Intent(this,ServiceActivity.class);
+            Intent intent = new Intent(this, ServiceActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_user) {
 
-            Intent intent = new Intent(this,UserlistActivity.class);
+            Intent intent = new Intent(this, UserlistActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_tools) {
 
-        }else if (id == R.id.nav_location){
+        } else if (id == R.id.nav_location) {
 
-            Intent intent = new Intent(this,MapsActivity.class);
+            Intent intent = new Intent(this, MapsActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_contact) {
 
-            Intent intent = new Intent(this,AllContactsActivity.class);
+            Intent intent = new Intent(this, AllContactsActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_websevices){
+        } else if (id == R.id.nav_websevices) {
 
-            Intent intent = new Intent(this,WebServicesActivity.class);
+            Intent intent = new Intent(this, WebServicesActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_logout) {
