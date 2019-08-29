6e237ef9c136b23b1c0b2ca0ada35f0c34e6d168
Kishan Hothi <skhothisurat@gmail.com>
2019-08-29 18:05:30 +0530

    changes in java code for run app

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -84,12 +84,12 @@ public class DashboardActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.dashboard, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.dashboard, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
